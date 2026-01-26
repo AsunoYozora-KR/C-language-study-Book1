f00d5e46656c4b8766c347c4fd4cdb53f67c4833
AsunoYozora-KR <starboard13@naver.com>
2026-01-26 09:59:01 +0900

    Add files via upload

---
+++ chapter6-3.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+	float value = 2.1f;
+	printf("%f", value);
+}
---
+++ chapter6-5~11.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+	/* chapter6-5 Example:
+	
+	int data1 = -1;
+	unsigned int data2 = 4294967295;
+	printf("%d %u %d %u", data1, data1, data2, data2);*/
+
+	/* chapter6-6 Example:
+	
+	char data = -1;
+	printf("%d %u", data, data); */
+
+	/* chapter6-7 Example:
+	
+	int data1 = 10;
+	int data2 = 010;
+	int data3 = 0x10;
+
+	printf("%x %d %o", data1, data2, data3);*/
+
+	/* chapter6-8 Example:
+	
+	float data = 12.34f;
+	printf("%f %e %E", data, data, data);*/
+
+	/* chapter6-9 Example:
+	
+	int data = 7;
+	printf("[%d] [%5d]", data, data);*/
+
+	/*chapter6-10 Example:
+	
+	int data = 7;
+	printf("[%5d] [%05d] [%-5d]", data, data, data);*/
+
+	/*chapter6-11 Example:
+	
+	double data = 3.141592;
+	printf("[%f] [%.4f] [%8.4f] [%-8.4f]", data, data, data, data);*/
+
+}
