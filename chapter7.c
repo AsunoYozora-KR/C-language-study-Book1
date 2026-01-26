57a441307fd69bd24f3816a150b6fb6584c36368
AsunoYozora-KR <starboard13@naver.com>
2026-01-26 11:28:01 +0900

    Add files via upload

---
+++ chapter7.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main()
+{
+	/*chapter7-1 Example:
+	
+	int data1, data2;
+	data1 = 5;
+	data2 = data1;
+	printf("data1 = %d, data2 = %d\n", data1, data2);*/
+
+
+	/*chapter7-2 Example:
+	
+	int data1 = 5, data2 = 3;
+
+	int result1 = data1 + data2;
+	int result2 = data1 * data2;
+	int result3 = data1 / data2;
+	int result4 = data1 % data2;
+
+	printf("result: %d %d %d %d", result1, result2, result3, result4);*/
+
+	/*chapter7-3 Example:
+	
+	int data1 = 5, data2 = 3;
+
+	int result1 = data1 > 7;
+	int result2 = data2 <= data1;
+	int result3 = data2 == 7;
+	int result4 = data2 != data1;
+
+	printf("result: %d %d %d %d\n", result1, result2, result3, result4);*/
+
+	/*chapter7-4 Example:
+	
+	int data1 = 5, data2 = 3;
+
+	int result1 = 0 || 1;
+	int result2 = 3 && -1;
+	int result3 = data1 == 3 || data2 == 3;
+	int result4 = data1 == 3 && data2 == 3;
+	int result5 = !data1;
+
+	printf("result: %d %d %d %d %d\n", result1, result2, result3, result4, result5);*/
+
+
+}
