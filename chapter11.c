e327f94ddd0a0f3cc8845fe824c6e85d2fefc8f7
AsunoYozora-KR <starboard13@naver.com>
2026-02-07 10:14:25 +0900

    Add files via upload

---
+++ chapter11.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/*chapter11-1 Example:
+
+int sum(int data1, int data2) {
+	int result = data1 + data2;
+	return result;
+}
+
+int main()
+{
+	int result;
+	result = sum(3, 5);
+	printf("3 + 5 =  %d", result);
+}*/
+
+/*chapter11-2 Example:
+
+int result;
+
+int Sum(int data1, int data2) {
+	result = data1 + data2;
+}
+int main() {
+	Sum(5, 3);
+	printf("5 + 3 = %d", result);
+ }*/
+
+/*chapter11-3 Example:
+
+int result;
+
+void Sum(int data1,int data2) {
+	int result;
+	result = data1 + data2;
+}
+
+int main() {
+	Sum(5, 3);
+	printf("5 + 3 = %d", result);
+}*/
+
